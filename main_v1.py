b412248689a53cfe26ffbfc0b38871b63a9a1afa
wangtuosyq <wangtuo195@gmail.com>
2019-11-06 22:50:17 +0800

    Add files via upload

--- main_v1.py
+++ main_v1.py
@@ -13,11 +13,12 @@ import tensorflow as tf
 import sklearn as skt
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.datasets import dump_svmlight_file
 class data_prepare(object):
     def __init__(self,path='./data/'):
         self.building_metadata=pd.read_csv(path+'building_metadata.csv')
         self.sample_submission=pd.read_csv(path+'sample_submission.csv')
-        self.test=pd.read_csv(path+'test.csv')
+        self.test=pd.read_csv(path+'test.csv',index_col='row_id')
         self.train=pd.read_csv(path+'train.csv')
         self.weather_test=pd.read_csv(path+'weather_test.csv')
         self.weather_train=pd.read_csv(path+'weather_train.csv')
@@ -30,6 +31,23 @@ class data_prepare(object):
         train_data['month']=train_data['timestamp'].apply(lambda x:x.split(' ')[0].split('-')[1]).apply(int)
         train_data['day']=train_data['timestamp'].apply(lambda x:x.split(' ')[0].split('-')[2]).apply(int)
         return train_data
+    def get_train_data_all(self):
+        train_data=pd.merge(left=self.train,right=self.building_metadata,how='left')
+        train_data=pd.merge(left=train_data,right=self.weather_train,how='left')
+        train_data['primary_use']=pd.factorize(train_data.primary_use)[0]
+        train_data['wday']=train_data['timestamp'].apply(lambda x:time.strptime(x,'%Y-%m-%d %H:%M:%S')[-3]) 
+        train_data['month']=train_data['timestamp'].apply(lambda x:x.split(' ')[0].split('-')[1]).apply(int)
+        train_data['day']=train_data['timestamp'].apply(lambda x:x.split(' ')[0].split('-')[2]).apply(int)
+        return train_data  
+
+    def get_test_data_all(self):
+        test_data=pd.merge(left=self.test,right=self.building_metadata,how='left')
+        test_data=pd.merge(left=test_data,right=self.weather_test,how='left')
+        test_data['primary_use']=pd.factorize(test_data.primary_use)[0]
+        test_data['wday']=test_data['timestamp'].apply(lambda x:time.strptime(x,'%Y-%m-%d %H:%M:%S')[-3]) 
+        test_data['month']=test_data['timestamp'].apply(lambda x:x.split(' ')[0].split('-')[1]).apply(int)
+        test_data['day']=test_data['timestamp'].apply(lambda x:x.split(' ')[0].split('-')[2]).apply(int)
+        return test_data      
     def get_train_data_daily(self,train_data):
         train_data['date']=train_data['timestamp'].apply(lambda x:x.split(' ')[0])
         train_data_daily=pd.DataFrame()
@@ -103,7 +121,7 @@ class model_train(object):
     def base_train_cell_v1(self,train_data,params):
         #params=self.init_common_params
         y=train_data.meter_reading
-        x=train_data.drop(labels=['building_id','meter','timestamp','site_id'],axis=1)
+        x=train_data.drop(labels=['building_id','meter','meter_reading','timestamp','site_id'],axis=1)
         x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2, random_state=1)
         xgb_val = xgb.DMatrix(x_test,label=y_test)
         xgb_train = xgb.DMatrix(x_train, label=y_train)
@@ -180,5 +198,57 @@ def test():#测试函数，保存实验中间过程
             
             data=train_data_daily[(train_data_daily.date<'2016-08-30')&(train_data_daily.date>'2016-06-20')])
         
-        
-        
+def test2():
+     self=data_prepare()
+     train_data=self.get_train_data_all()
+     del self
+     init_common_params= {
+                'booster': 'gblinear',
+                'objective': 'reg:linear',  
+                #'num_class': 10,               # 类别数，与 multisoftmax 并用
+                'gamma': 0.1,                  # 用于控制是否后剪枝的参数,越大越保守，一般0.1、0.2这样子。
+                'max_depth': 3,               # 构建树的深度，越大越容易过拟合
+                'lambda': 0.5,                   # 控制模型复杂度的权重值的L2正则化项参数，参数越大，模型越不容易过拟合。
+                'subsample': 0.7,              # 随机采样训练样本
+                'colsample_bytree': 0.7,       # 生成树时进行的列采样
+                'min_child_weight': 3,
+                'silent': 0,                   # 设置成1则没有运行信息输出，最好是设置为0.
+                'eta': 0.007,                  # 如同学习率
+                #'seed': 1000,
+                #'nthread': 4,                  # cpu 线程数，默认为最大可用线程数
+                }  
+     y=train_data.meter_reading
+     x=train_data.drop(labels=['meter_reading','timestamp'],axis=1)
+     x=x.fillna(-999)
+     del train_data
+     x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2, random_state=1)
+     del x,y
+     dump_svmlight_file(x_test,y_test,'xgb_test.lib')
+     del x_test, y_test
+     dump_svmlight_file(x_train,y_train,'xgb_train.lib')
+     
+     del x_train, y_train
+     #xgb_val = xgb.DMatrix(x_test,label=y_test)
+
+     #del x_test,y_test,xgb_val
+     
+     #xgb_train = xgb.DMatrix(x_train, label=y_train)
+     #xgb_train.save_binary('xgb_train')
+     #del x_train,y_train,xgb_train
+     xgb_val=xgb.DMatrix(data = 'xgb_test.lib#xgb_val.cache')
+     xgb_train=xgb.DMatrix(data = 'xgb_train.lib#xgb_train.cache')
+     params=init_common_params
+     plst = params.items()
+     num_rounds = 5000 # 迭代次数
+     watchlist = [(xgb_train, 'train'),(xgb_val, 'val')]
+     model = xgb.train(plst, xgb_train, num_rounds, watchlist,early_stopping_rounds=50)
+     print(model.eval(xgb_val))
+     y_predict=pd.DataFrame(model.predict(xgb_val),columns=['meter_reading'])
+     y_predict['type']='predict'
+     #y_test.index=range(len(y_test))
+     #Y_test=pd.DataFrame(y_test,columns=['meter_reading'])
+     #Y_test['type']='test'   
+     #data=pd.concat([y_predict,Y_test])
+     #data['index']=data.index
+     #sns.relplot(x='index', y='meter_reading', kind='line',hue='type', data=data[(data['index']<1000)&(data['index']>800)])
+          
